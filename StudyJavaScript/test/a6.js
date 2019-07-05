d40a03516ae77739a97e5a051d6bbf455be32b9b
Developerjjj <triplej03@nate.com>
2019-07-06 03:02:50 +0900

    Answer 6

---
+++ StudyJavaScript/test/a6.js
@@ -0,0 +1,8 @@
+var data = {
+    "id" : "JJJ",
+    "password" : "qawsedrf12345"
+}
+// email이라는 property를 추가하는 방식에는
+// .을 이용하는 방법과 []를 이용하는 방법이 있다.
+data.email = "triplej@yahoo.com";
+data['email'] = "triplej@yahoo.com";
