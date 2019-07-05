580bda3ac919d158e5985ba98603afc49174ce0b
Developerjjj <triplej03@nate.com>
2019-07-06 03:09:03 +0900

    Answer 7

---
+++ StudyJavaScript/test/a7.js
@@ -0,0 +1,11 @@
+var arr = ['a', 'b', 'c', 'd'];
+
+function foo(input) 
+{
+    var output = {}
+    output['a'] = 1;
+    output['b'] = 2;
+    output['c'] = 3;
+    output['d'] = 4;
+    return output;
+}
