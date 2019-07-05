98fcf3a651f5e7a09c45a5b98a3f2edbc4cee5ef
Developerjjj <triplej03@nate.com>
2019-07-06 03:25:32 +0900

    Answer 8

---
+++ StudyJavaScript/test/a8.js
@@ -0,0 +1,10 @@
+var List = [];
+function putSchedule(whatToDo) {
+    list.push(whatToDo);
+}
+function deleteSchedule(Done) { 
+    list.remove(Done);
+}
+function deleteAll(List) { 
+    list = [];
+}
