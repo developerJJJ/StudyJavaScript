9e1f58aadbbd423d2ecf2e6bc980eb8c552deb61
Developerjjj <triplej03@nate.com>
2019-07-06 11:01:54 +0900

    fix a7

---
+++ StudyJavaScript/test/a17.js
@@ -0,0 +1,8 @@
+
+배열 `funcs` 는 인덱스 `i` 에서 `i+1`를 출력하는 **익명함수**가 있다. `funcs`의 길이가 3 일 때, `funcs` 를 작성하고, 각각의 익명함수를 실행하는 코드를 작성하시오.
+
+var funcs = [
+    foo = function(){
+        console.log(i+1);
+    }
+]
--- StudyJavaScript/test/a7.js
+++ StudyJavaScript/test/a7.js
@@ -3,9 +3,12 @@ var arr = ['a', 'b', 'c', 'd'];
 function foo(input) 
 {
     var output = {}
-    output['a'] = 1;
-    output['b'] = 2;
-    output['c'] = 3;
-    output['d'] = 4;
-    return output;
+    iLength = input.length;
+    for(i=0; i<iLength; i++)
+    {
+        output[input[i]] = i+1;
+    }
+    // return output;
+    console.log(output);
 }
+foo(arr);
