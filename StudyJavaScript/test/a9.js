e105180db76ff918ef59d07dd2de7e18d3d93aee
Developerjjj <triplej03@nate.com>
2019-07-06 12:20:00 +0900

    submit

--- StudyJavaScript/test/a9.js
+++ StudyJavaScript/test/a9.js
@@ -1,16 +1,73 @@
-StringA = "AHDBGKKSHEFV"
+stringA = "AHDBGKKSHEFV"
 
-function al1phabetToNumber(str){
-    a = str.charCodeAt(0) - 97
-    return result;
+function alphabetToNumber(str){
+    result = 0;
+
+    someObject = {}
+
+    for(i=0; i<str.length; i++)
+    {
+        c = str[i];
+        someObject[c] = alphabetToNumber(c);
+    }
+    return someObject;
+    // str.charCodeAt(0) - 64
 }
-
+/*
 + 문제 9-1.  `switch-case`문을 이용하여 해결하시오.
-switch(){
-case 0;
+
+function alphabetToNumber(str){
+    result = 0;
+
+    someObject = {}
+
+    for(i=0; i<str.length; i++)
+    {
+        c = str[i];
+        someObject[c] = alphabetToNumber(c);
+    }
+
+
+    function "caseInSwitch(val){
+    var answer = "";
+    switch(val){
+        case 'A':
+            answer = 1;
+        break;
+        case 'B':
+            asnwer = 2;
+        break;
+        case 'D':
+            asnwer = 4;
+        break;
+        case 'E':
+            asnwer = 5;
+        break;
+        case 'F':
+            asnwer = 6;
+        break;
+        case 'G':
+            asnwer = 7;
+        break;
+        case 'H':
+            asnwer = 8;
+        break;
+        case 'K':
+            asnwer = 2;
+        break;
+    }
+    return answer;
 }
 
-+ 문제 9-2. 객체(`{}`)를 이용하여 해결하시오.
-data = {
-    "A" : 1
+    return someObject;
+    // str.charCodeAt(0) - 64
 }
+
+caseInSwitch(1);
+*/
+
+// + 문제 9-2. 객체(`{}`)를 이용하여 해결하시오.
+
+// console.log(stringA.charCodeAt(0));
+// console.log(someObject);
+alphabetToNumber(stringA);
